1f29de519ff0cb5b53a9f0e4bf2690be0920480d
Crowtit517 <pangkan517@gmail.com>
2023-09-04 22:08:19 +0900

    test 0904

--- C-project/프로그램의 기본구성.c
+++ C-project/프로그램의 기본구성.c
@@ -2,9 +2,15 @@
 int main(void) {
 
 	printf("Hello world! \n");
-	printf("Hello test\n");
+	
+	//3X + 4 = Y   x에 2를 대입하면 y는 얼마인가? 10이다. x는 입력 y는 출력이라 한다.
+	//적절한 입력과 그에 따른 출력이 존재하는 것을 가르켜 '함수(function)'이라 한다.
+	//int - 출력형태 | main - 함수이름 | (void) - 입력형태
+	//int main(void){ 함수의 몸체(body) }
 
-
+	printf("Hello Everybody\n");
+	printf("%d\n", 1234);
+	printf("%d %d\n", 10, 20);
 
 	return 0;
 }
